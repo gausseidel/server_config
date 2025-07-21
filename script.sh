e23db68d31e16ddf742150eea07564a25a67c316
d4vi <faberd@protonmail.com>
2025-07-21 12:16:05 +0200

    added script

---
+++ script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+# Installa i pacchetti necessari
+apt update
+apt install -y tmux fish stow
+
+# Verifica se nvim è già presente
+if ! command -v nvim >/dev/null 2>&1; then
+  echo "Neovim non trovato, procedo con l'installazione..."
+
+  # Scarica neovim
+  wget https://github.com/neovim/neovim/releases/download/v0.11.3/nvim-linux-x86_64.tar.gz
+
+  # Estrai
+  tar xzvf nvim-linux-x86_64.tar.gz
+
+  # Sposta e crea il link simbolico
+  mv nvim-linux-x86_64 /opt/nvim
+  ln -sf /opt/nvim/bin/nvim /usr/local/bin/nvim
+
+  # Pulizia file temporanei
+  rm nvim-linux-x86_64.tar.gz
+else
+  echo "Neovim già installato, salto il download."
+fi
+
+# Rimuove configurazioni precedenti
+rm -rf /root/.config /root/.tmux.conf
+
+# Aggiorna configurazione da git e applica con stow
+cd /root/server_config/
+git pull
+stow .
