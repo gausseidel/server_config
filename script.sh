518b848ccfd0bf394710e3f1e2d76fa7b613ca08
d4vi <faberd@protonmail.com>
2025-07-24 02:26:40 +0200

    fix link to conf

--- script.sh
+++ script.sh
@@ -2,8 +2,11 @@
 set -e
 
 sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
+mkdir -p ~/.config/nixpkgs
+ln -s /root/server_config/nix/ ~/.config/nixpkgs/
 
 home-manager switch
 
