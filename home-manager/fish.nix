b66dbd4d0dd652ce96ad651bc9271421110ccc80
d4vi <faberd@protonmail.com>
2025-07-24 19:14:04 +0200

    fix alias

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -30,7 +30,7 @@
       s = "systemctl";
       st = "systemctl status";
       sr = "systemctl restart";
-      update = "cd /root/server_config; git pull; home-manager switch";
+      update = "cd $HOME/server_config; git pull; home-manager switch";
     };
   };
 }
