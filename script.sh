e562f011290b3618a0818431e88d69ca25f32feb
d4vi <faberd@protonmail.com>
2025-07-24 02:21:39 +0200

    added home-manager

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 set -e
 
 sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
-
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
 nix-channel --update
+
 home-manager switch
 
