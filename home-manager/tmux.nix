419a987896b402731e0fec5a484c81a952ca4c6a
d4vi <faberd@protonmail.com>
2025-07-24 12:37:19 +0200

    update

--- home-manager/tmux.nix
+++ home-manager/tmux.nix
@@ -3,10 +3,9 @@
 {
   programs.tmux = {
     enable = true;
-
+    shell = "${pkgs.fish}/bin/fish";
     terminal = "tmux-256color";
     extraConfig = ''
-      set-option -g default-shell ${pkgs.fish}/bin/fish
       set-option -ga terminal-overrides ",xterm-256color:Tc"
       set-option -g window-status-style bg=default
       set-option -g status-style bg=default,fg=default
