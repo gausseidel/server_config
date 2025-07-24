f06f32d65168a398e96190e1b450c9c74197e27f
d4vi <faberd@protonmail.com>
2025-07-24 19:05:34 +0200

    added user function

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -10,6 +10,18 @@
       starship init fish | source
     '';
 
+    initExtra = ''
+      function user
+          set home_users (ls /home)
+          if count $home_users > 1
+              echo "Errore: ci sono più utenti sotto /home. Non so quale usare."
+              return 1
+          end
+          set target_user $home_users[1]
+          su - $target_user
+      end
+    '';
+    
     shellAliases = {
       ls = "lsd -l --color=auto";
       log = "journalctl -xe";
