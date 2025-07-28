d306261f49d8950a3f20dc6d75ad480e8701eab3
d4vi <faberd@protonmail.com>
2025-07-28 21:49:06 +0200

    udpate

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -23,25 +23,20 @@ in
   imports = [ ./fish.nix ./tmux.nix ];
 
 
-home.file.".profile" = {
-  text = ''
-    # Se siamo root (login SSH)
-    if [ "$(id -u)" -eq 0 ]; then
-      # Se non siamo già dentro tmux e siamo su terminale SSH
-      if [ -z "$TMUX" ] && [ -n "$SSH_TTY" ]; then
-        # Provo ad attaccare la sessione ssh_tmux, se fallisce ne creo una nuova
-        tmux attach-session -t ssh_tmux 2>/dev/null || tmux new-session -s ssh_tmux
-      fi
-
-    # Se siamo un utente normale (da su - user)
-    else
-      # Se non siamo già dentro tmux e la shell è interattiva
-      if [ -z "$TMUX" ] && [ -t 0 ]; then
-        tmux attach-session -t user_tmux 2>/dev/null || tmux new-session -s user_tmux
-      fi
-    fi
-  '';
-};
+  home.file.".profile" =
+    if user == "root" then {
+      text = ''
+        if [ -z "$TMUX" ] && [ -n "$SSH_TTY" ]; then
+          tmux attach-session -t ssh_tmux 2>/dev/null || tmux new-session -s ssh_tmux
+        fi
+      '';
+    } else {
+      text = ''
+        if [ -z "$TMUX" ] && [ -t 0 ]; then
+          tmux attach-session -t user_tmux 2>/dev/null || tmux new-session -s user_tmux
+        fi
+      '';
+    };
 
 
   home.file.".config/starship.toml".source = ./starship.toml;
