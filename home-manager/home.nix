5b780857cd60a304d0bffe9aac2a476749d3dae8
d4vi <faberd@protonmail.com>
2025-07-28 21:44:54 +0200

    update

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -24,21 +24,21 @@ in
 
 
   home.file.".profile" =
-    if user == "root" then {
-      text = ''
-        if [[ -z "$TMUX" && -n "$SSH_TTY" ]]; then
-            tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
-        fi
-      '';
-    } else {
-      text = ''
-        if [[ -n "$TMUX" ]]; then
-            tmux detach-client
-        fi
+# Se siamo root (login SSH)
+    if [ "$(id -u)" -eq 0 ]; then
+      # Se non siamo già dentro tmux e siamo su terminale SSH
+      if [ -z "$TMUX" ] && [ -n "$SSH_TTY" ]; then
+        # Provo ad attaccare la sessione ssh_tmux, se fallisce ne creo una nuova
+        tmux attach-session -t ssh_tmux 2>/dev/null || tmux new-session -s ssh_tmux
+      fi
 
-        if [[ -z "$TMUX" && -t 0 ]]; then
-            tmux attach-session -t user_tmux || tmux new-session -s user_tmux
-        fi
+# Se siamo un utente normale (da su - user)
+    else
+      # Se non siamo già dentro tmux e la shell è interattiva
+      if [ -z "$TMUX" ] && [ -t 0 ]; then
+        tmux attach-session -t user_tmux 2>/dev/null || tmux new-session -s user_tmux
+      fi
+    fi
       '';
     };
 
