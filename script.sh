6c2c55970a3efd1314c8ef6a9d372df69457acde
d4vi <faberd@protonmail.com>
2025-07-31 01:23:19 +0200

    added profile

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 REPO_DIR="/root/server_config"
 CONFIG_DIR="/root/.config"
 
-apt install neovim fish zoxide tmux stow htop curl
+apt install neovim fish zoxide tmux stow htop curl lsd
 mkdir -p $CONFIG_DIR
 stow --dir=$REPO_DIR --target=$CONFIG_DIR --adopt
 
