954704f6d79494a98cca6daae0a8b4374e7d4fe4
d4vi <faberd@protonmail.com>
2025-07-31 01:26:23 +0200

    update script

--- script.sh
+++ script.sh
@@ -5,6 +5,9 @@ REPO_DIR="/root/server_config"
 CONFIG_DIR="/root/.config"
 
 apt install neovim fish zoxide tmux stow htop curl lsd
+
 mkdir -p $CONFIG_DIR
 stow --dir=$REPO_DIR --target=$CONFIG_DIR --adopt
+ln -s $CONFIG_DIR/.profile $HOME
 
+curl -sS https://starship.rs/install.sh | sh
