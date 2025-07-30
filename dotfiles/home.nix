69f1397c634dbcce4ae11147f87fe6e6a08b2604
d4vi <faberd@protonmail.com>
2025-07-31 01:09:48 +0200

    added file

---
+++ dotfiles/home.nix
@@ -0,0 +1,56 @@
+{ config, pkgs, ... }:
+
+let
+  user = builtins.getEnv "USER";
+  homeDir = builtins.getEnv "HOME";
+in
+{
+  home.username = user;
+  home.homeDirectory = homeDir;
+  home.stateVersion = "25.05";
+
+  home.packages = with pkgs; [
+    wget
+    curl
+    ripgrep
+    gcc
+    htop
+    tmux
+    bat
+    lsd
+  ];
+
+  imports = [ ./fish.nix ./tmux.nix ];
+
+
+  home.file.".profile" =
+    if user == "root" then {
+      text = ''
+        if [ -z "$TMUX" ] && [ -n "$SSH_TTY" ]; then
+          tmux attach-session -t ssh_tmux 2>/dev/null || tmux new-session -s ssh_tmux
+          logout
+        fi
+      '';
+    } else {
+      text = ''
+        if [ -z "$TMUX" ] && [ -t 0 ]; then
+          tmux attach-session -t user_tmux 2>/dev/null || tmux new-session -s user_tmux
+          exit
+        fi
+      '';
+    };
+
+
+  home.file.".config/starship.toml".source = ./starship.toml;
+
+  home.file.".config/nvim".source = ./nvim;
+  programs.neovim.enable = true;
+
+  programs.starship.enable = true;
+  programs.fish.enable = true;
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+}
--- script.sh
+++ script.sh
@@ -1,25 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "🔧 Verifica installazione di Nix..."
-if ! command -v nix &>/dev/null; then
-  echo "📥 Installo Nix..."
-  printf 'n\ny\n' | sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
-  source /etc/profile.d/nix.sh
-else
-  echo "✅ Nix già installato."
-fi
-
-echo "📦 Aggiunta del canale home-manager (se necessario)..."
-if ! nix-channel --list | grep -q '^home-manager'; then
-  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-fi
-
-echo "🔄 Aggiornamento dei canali..."
-nix-channel --update
-
-# Per info e debug
-#nix-shell -p nix-info --run "nix-info -m"
 
 echo "📁 Collegamento della configurazione in ~/.config/home-manager..."
 REPO_DIR="$HOME/server_config"
