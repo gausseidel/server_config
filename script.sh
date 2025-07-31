072b5ae32843e9dc08481c18397a15518b253dff
d4vi <faberd@protonmail.com>
2025-07-31 12:39:25 +0200

    added nvim install latest

--- script.sh
+++ script.sh
@@ -4,10 +4,18 @@ set -euo pipefail
 REPO_DIR="/root/server_config"
 CONFIG_DIR="/root/.config"
 
-apt install neovim fish zoxide tmux stow htop curl lsd
+# Install packages
+apt install fish zoxide tmux stow htop curl lsd
 
+# Link dotfiles
 mkdir -p $CONFIG_DIR
 stow --dir=$REPO_DIR --target=$CONFIG_DIR --adopt
 ln -s $CONFIG_DIR/.profile $HOME
 
-curl -sS https://starship.rs/install.sh | sh
+# Starship
+#curl -sS https://starship.rs/install.sh | sh
+
+# NeoVim
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage
+chmod +x nvim-linux-x86_64.appimage
+mv  nvim-linux-x86_64.appimage /usr/local/bin/nvim
