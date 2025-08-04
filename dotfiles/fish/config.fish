44a0222e74b4bbf471a624d5358f8f4c09adba00
d4vi <faberd@protonmail.com>
2025-08-04 23:09:43 +0200

    remove alias nano

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -20,7 +20,6 @@ alias ls 'lsd -l --group-directories-first'
 alias la 'lsd -la --group-directories-first'
 abbr -a log "journalctl -xe"
 abbr -a v "/usr/local/bin/squashfs-root/AppRun"
-alias nano "/usr/local/bin/squashfs-root/AppRun"
 alias grep "grep --color=auto"
 abbr -a s "systemctl"
 abbr -a st "systemctl status"
