f67a783274a88544a4d0475ed2ea558dbeaa6e76
d4vi <faberd@protonmail.com>
2025-08-04 12:15:37 +0200

    update

--- script.sh
+++ script.sh
@@ -11,12 +11,19 @@ apt install -y fish zoxide tmux stow htop curl lsd net-tools gcc
 mkdir -p $CONFIG_DIR
 cd $REPO_DIR/dotfiles
 stow . --target=$CONFIG_DIR --adopt
+
+rm $HOME/.profile
 ln -s $CONFIG_DIR/.profile $HOME
 
 # Starship
 #curl -sS https://starship.rs/install.sh | sh
 
-# NeoVim
-wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage
-chmod +x nvim-linux-x86_64.appimage
-mv  nvim-linux-x86_64.appimage /usr/local/bin/nvim
+if [ ! -x /usr/local/bin/nvim ]; then
+    echo "Neovim non trovato. Procedo con l'installazione..."
+    wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage
+    chmod +x nvim-linux-x86_64.appimage
+    mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
+    echo "Neovim installato in /usr/local/bin/nvim"
+else
+    echo "Neovim è già installato in /usr/local/bin/nvim"
+fi
