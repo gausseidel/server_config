5e10c838c71dfd000f21f606e574311ceb7607ff
d4vi <faberd@protonmail.com>
2025-09-28 19:10:56 +0200

    update

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -19,7 +19,7 @@ end
 alias ls 'lsd -l --group-directories-first'
 alias la 'lsd -la --group-directories-first'
 abbr -a log "journalctl -xe"
-alias v "/usr/local/bin/squashfs-root/AppRun"
+alias v "nvim"
 alias grep "grep --color=auto"
 abbr -a s "systemctl"
 abbr -a st "systemctl status"
